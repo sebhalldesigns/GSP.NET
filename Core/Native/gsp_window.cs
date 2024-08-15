425d522c85279ad88e28ff1008de08baabd0a17a
Seb Hall <sebhalldesigns@gmail.com>
2024-08-15 10:29:06 +0100

    created nuget package

---
+++ Core/Native/gsp_window.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GSP.NET.Core.Native;
+
+using gwindow_t = UIntPtr;
+using gstring_t = string;
+
+
+public static class gsp_window {
+
+    [DllImport("GSPCore.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void gsp_window_poll_events();
+
+    [DllImport("GSPCore.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool gsp_window_is_window_valid(gwindow_t window);
+
+    [DllImport("GSPCore.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern UIntPtr gsp_window_create_window();
+
+    [DllImport("GSPCore.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void gsp_window_set_title(gwindow_t window, gstring_t title);
+
+}
